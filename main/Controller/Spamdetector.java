6af403546c2e4832484382ce2d9637f3c7e1f385
dannynoordamdev <dannynoordam.dev@gmail.com>
2023-03-28 12:01:32 +0200

    Added all the classes, no code yet.

---
+++ main/Controller/Spamdetector.java
@@ -0,0 +1,17 @@
+public class Spamdetector {
+    private String api_key;
+    private String database_url;
+
+    private String userInput;
+    private int userInputInt;
+
+    public boolean checkIfInputIsSpam(){
+        return true;
+
+    }
+
+    public void checkFormatUserInput(){
+        }
+
+    }
+
---
+++ main/View/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args){
+    Spamdetector test1 = new Spamdetector();
+    }
+}
